cdd3525ae84bf81d53e19e8562840f3973fc5fcc
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-08-29 15:41:52 -0400

    ahoy m8s

---
+++ querypractice/Scripts/moving.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    $(".btn1").click(function () {
+        $("i2").hide(2000);
+    });
+    $(".btn2").click(function () {
+        $("p").show(2000);
+    });
+});
+
+function feel() {
+     var store = document.getElementById('real').value;
+
+     document.getElementById('work').innerHTML = "Hello Joe " + store;
+};
+
+
