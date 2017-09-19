fab37bf35f3a9488c907df35bc6b97bfb0d1dfbe
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-09-19 13:24:09 -0400

    asp.net

---
+++ MVCMovie/MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID is: " + ID);
+        }
+    }
+}
