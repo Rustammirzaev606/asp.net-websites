1102da8fcbd292775ca3f8873151cc6d559188ac
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-09-22 15:50:30 -0400

    Saving before trying to break it 1

---
+++ DunkMemes/DunkMemes/Models/Customers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+
+namespace DunkMemes.Models
+{
+    public class Customers
+    {        
+        public int ID { get; set; }
+        [Display(Name = "First Name"), Required, StringLength(10, MinimumLength = 3)]
+        public string FIrstName { get; set; }
+        [Display(Name = "Last Name"), Required, StringLength(10, MinimumLength = 3)]
+        public string LastName { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Range(18, 95)]
+        public int Age { get; set; }
+    }
+
+    public class CustomersDbContext : DbContext
+    {
+        public DbSet<Customers> Custies { get; set; }
+
+    }
+
+}
